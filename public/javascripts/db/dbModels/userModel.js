e6e01c05cbbde37387da1752c96d5d1b63bab3cb
David Ross <officialdavidross@gmail.com>
2024-03-19 16:26:53 -0700

    Install passport. Update user model with passport-local-mongoose

--- public/javascripts/db/dbModels/userModel.js
+++ public/javascripts/db/dbModels/userModel.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema(
     {
-        username: {
+        displayname: {
             type: String,
             required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            min: 8,
+            default: 'Welcome'
         },
         notes: {
             type: String,
@@ -22,5 +18,5 @@ const userSchema = new Schema(
     }
 );
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', userSchema);
