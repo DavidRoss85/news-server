3ca038ffeb572c563ada86e141e5cefc0890079c
David Ross <officialdavidross@gmail.com>
2024-04-17 17:58:19 -0700

    Disabled Redis caching due to google cloud VPC requirements

--- js/DEFAULTS.js
+++ js/DEFAULTS.js
@@ -1,7 +1,7 @@
 module.exports.TOKEN_TTL = 86400000; // ONE DAY
 module.exports.ONE_DAY = 86400000;
 module.exports.LOG_PATH = '';
-module.exports.CACHE_BAN_TIME = 'BANNED_TIME';
+module.exports.CACHE_ON = false;
 module.exports.ERROR_NEWS = {
     "status": "error",
     "message": "error",
--- js/redisClient.js
+++ js/redisClient.js
@@ -1,11 +1,12 @@
 //This module is used to cache results
-const { CACHE_BAN_TIME, ONE_DAY } = require('./DEFAULTS');
+const { ONE_DAY, CACHE_ON } = require('./DEFAULTS');
+const BAN_KEY = 'BANNED_TIME';
 const handleError = require('./handleError');
 
 const { createClient } = require('redis');
 const client = createClient({
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
+    // username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_DEFAULT_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT
@@ -13,11 +14,15 @@ const client = createClient({
 });
 
 client.on('error', (err) => { handleError(err, 'redisClient') });
+client.on('ready',()=>{ console.log('Connected to Redis host');});
+client.on('end',()=>{ console.log('Connection to Redis ended');});
 
 module.exports.connectToCache = async () => {
+    if(!CACHE_ON){
+        return;
+    };
     try{
         await client.connect();
-        console.log('Connected to Redis host');
     }catch(err){
         handleError(err,'redisClient/connectToCache');
         console.log('Failed to connect to Redis host...')
@@ -25,6 +30,9 @@ module.exports.connectToCache = async () => {
 }
 
 module.exports.checkCache = async (key) => {
+    if(!CACHE_ON){
+        return null;
+    };
     try {
         let cacheItem = await client.get(key);
         cacheItem = cacheItem ? await JSON.parse(cacheItem) : null;
@@ -37,6 +45,9 @@ module.exports.checkCache = async (key) => {
 };
 
 module.exports.writeCache = async (key, data) => {
+    if(!CACHE_ON){
+        return;
+    };
     const stringData = JSON.stringify(data);
     try {
         // key='test'; data="{random: 'information'}"
@@ -49,8 +60,11 @@ module.exports.writeCache = async (key, data) => {
 };
 
 module.exports.checkLimit = async () => {
+    if(!CACHE_ON){
+        return 'none';
+    };
     try {
-        const banTime = await client.get(CACHE_BAN_TIME);
+        const banTime = await client.get(BAN_KEY);
         const timeLeft = Date.now() - banTime;
 
         // console.log('\nThis is the fetched banTime Value: ', banTime);
@@ -65,16 +79,19 @@ module.exports.checkLimit = async () => {
     } catch (err) {
         handleError(err, 'redisClient/checkLimit');
         return null
-    }
+    };
 };
 
 module.exports.setLimit = async () => {
+    if(!CACHE_ON){
+        return;
+    };
     try {
-        await client.set(CACHE_BAN_TIME, Date.now());
+        await client.set(BAN_KEY, Date.now());
         console.log('**Set Limit success**')
     } catch (err) {
         handleError(err, 'redisClient/setLimit')
-    }
+    };
 };
 
 
