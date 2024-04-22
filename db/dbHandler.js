c85f49c002bc52ecc25d16d52c82a6e1917a0646
David Ross <officialdavidross@gmail.com>
2024-04-22 13:02:45 -0700

    Updated Mongo URL

--- db/dbHandler.js
+++ db/dbHandler.js
@@ -1,5 +1,5 @@
-// const url = process.env.MONGO_SERVER_URL //'mongodb://127.0.0.1:27017/newsFeedData';
-const url = 'mongodb://127.0.0.1:27017/newsFeedData';
+const url = process.env.MONGO_SERVER_URL //'mongodb://127.0.0.1:27017/newsFeedData';
+// const url = 'mongodb://127.0.0.1:27017/newsFeedData';
 const mongoose = require('mongoose');
 const passport = require('passport');
 const authenticate = require('../authenticate')
