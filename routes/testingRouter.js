f62500097202fbe8e25d0d985b41cc341cbf6389
David Ross <officialdavidross@gmail.com>
2024-04-22 13:30:01 -0700

    Removed cache testing endpoints

--- routes/testingRouter.js
+++ routes/testingRouter.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const testingRouter = express.Router();
 const dbHandler = require('../db/dbHandler');
-const authenticate = require('../authenticate');
-const passport = require('passport');
-const handleError = require('../js/handleError');
-const { systemLog } = require('../js/logHandler');
+// const authenticate = require('../authenticate');
+// const passport = require('passport');
+// const handleError = require('../js/handleError');
 const { cors, corsWithOptions } = require('./corsModule');
-const cacheHandler = require('../js/redisClient');
 
 const rejectUser = (res) => {
     res.statusCode = 403
@@ -25,66 +23,6 @@ testingRouter.route('/dbconnectionstatus')
             res.statusCode = 200
             res.setHeader('Content-Type', 'application/json');
             res.json(result);
-            return;
-        });
-
-testingRouter.route('/writeCache')
-    .options(corsWithOptions, (req, res) => res.sendStatus(200))
-    .get(cors, authenticate.verifyUser,
-        async (req, res) => {
-            if (!req.user.admin) {
-                rejectUser();
-                return;
-            }
-            cacheHandler.writeCache('MyEntry', 'MyData!!')
-            const result = { test: 'success' }
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application/json');
-            res.json(result);
-            return;
-        });
-
-testingRouter.route('/readCache')
-    .options(corsWithOptions, (req, res) => res.sendStatus(200))
-    .get(cors, authenticate.verifyUser,
-        async (req, res) => {
-            if (!req.user.admin) {
-                rejectUser();
-                return;
-            }
-            const result = await cacheHandler.readCache('testResults')
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application/json');
-            res.json(result);
-            return;
-        });
-testingRouter.route('/saveCache')
-    .options(corsWithOptions, (req, res) => res.sendStatus(200))
-    .get(cors, authenticate.verifyUser,
-        async (req, res) => {
-            if (!req.user.admin) {
-                rejectUser();
-                return;
-            }
-            cacheHandler.saveCache()
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application/json');
-            res.json({ result: 'executed' });
-            return;
-        });
-
-testingRouter.route('/importCache')
-    .options(corsWithOptions, (req, res) => res.sendStatus(200))
-    .get(cors, authenticate.verifyUser,
-        async (req, res) => {
-            if (!req.user.admin) {
-                rejectUser();
-                return;
-            }
-            cacheHandler.importCache()
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application/json');
-            res.json({ result: 'executed' });
             return;
         });
 
